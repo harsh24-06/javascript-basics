a5a91668f2caea368b852fa8c23eb0fe6d3c2e88
Harsh Golchha <golchhaharsh497@gmail.com>
2025-03-19 20:36:26 +0530

    DataTypes Tutorial

---
+++ Basic/03_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)// we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Hitesh") Print Hitesh
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+// let flag=true;
+// console.log(typeof flag)
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique Gives Unique Identifier
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof Symbol);
+
