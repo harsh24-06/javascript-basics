2a5ed76436d1421faf525e7784e9e8b3cbb27284
Harsh Golchha <golchhaharsh497@gmail.com>
2025-03-19 21:42:44 +0530

    Conversion Operator

---
+++ 04_ConversionOperator.js
@@ -0,0 +1,65 @@
+// let score = "hitesh"
+let score="33";//Taken As a string
+//console.log(typeof score); String
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)//Convert into integer
+// console.log(typeof valueInNumber); //number
+console.log(valueInNumber); //NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); 12
+// console.log(1 + "2"); 12 
+// console.log("1" + 2 + 2); 122
+// console.log(1 + 2 + "2"); 32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); 1
+// console.log(+""); 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
